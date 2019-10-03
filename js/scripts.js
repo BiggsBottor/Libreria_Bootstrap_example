b512e931e9572b00716f6824a93d7a353c7ee994
BiggsBottor <jonatan.lledo@gmail.com>
2019-10-03 13:04:51 +0200

    Finalizado edición con Bootstrrap 4.3 + CSS + HTML + JQuery/Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
+//animacíon para cambiar el color del logo, intercambiando entre 2 imágenes
 $(document).ready(function() {
-    $("#Logo-link-id").hover(function() {
+    $("#Logo-link-id").hover(function() { //hover in
             $("#Logo-img-id").attr("src", "images/logo-la-llibreria-hover.png");
         },
-        function() {
+        function() { //hover out
             $("#Logo-img-id").attr("src", "images/logo-la-llibreria.png");
         });
 });
